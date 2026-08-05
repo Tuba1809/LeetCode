d40e8ac0d50e3c51b7f129873e76823669823261
Tuba Gulfen Qureshi <tuba.24bai10796@vitbhopal.ac.in>
2026-08-05 21:56:19 +0530

    Time: 0 ms (100%), Space: 42.1 MB (74.58%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int teams=n;
+        int matches=0;
+        while (teams>1){
+            if (teams%2==0){
+                matches+=teams/2;
+                teams=teams/2;
+            }
+            else{
+                matches+=(teams-1)/2;
+                teams=((teams-1)/2)+1;
+            }
+        }
+        return matches;
+    }
+}
