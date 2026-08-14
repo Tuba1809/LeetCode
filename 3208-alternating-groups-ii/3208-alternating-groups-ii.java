5d691fbc90f39f51f661339edc488317202eaa3b
Tuba Gulfen Qureshi <tuba.24bai10796@vitbhopal.ac.in>
2026-08-14 23:41:56 +0530

    Time: 5 ms (67.14%), Space: 100.8 MB (57.98%) - LeetHub

---
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int n = colors.length;
+        int count = 0;
+        int len = 1;
+        for (int i = 1; i < n + k - 1; i++) {
+            if (colors[i % n] != colors[(i - 1) % n]) {
+                len++;
+            } else {
+                len = 1;
+            }
+            if (len >= k && i - k + 1 < n) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
